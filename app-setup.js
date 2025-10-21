d6c13b26c19c9ee6b163f204b6874c4256779723
r70732950-web <r70732950@gmail.com>
2025-10-22 00:31:59 +0300

    Add files via upload
    Signed-off-by: r70732950-web <r70732950@gmail.com>

--- app-setup.js
+++ app-setup.js
@@ -32,12 +32,8 @@ window.globalAdminTools = {};
 export const productsCollection = collection(db, "products");
 export const categoriesCollection = collection(db, "categories");
 export const announcementsCollection = collection(db, "announcements");
-export const promoCardsCollection = collection(db, "promo_cards"); // Needed for old logic if any
-export const brandsCollection = collection(db, "brands"); // Needed for old logic if any
-// ====== کۆدی زیادکراو بۆ چارەسەرکردنی کێشە ======
 export const promoGroupsCollection = collection(db, "promo_groups");
 export const brandGroupsCollection = collection(db, "brand_groups");
-// ===============================================
 
 // Translations Export
 export const translations = {
@@ -384,4 +380,3 @@ export const policiesForm = document.getElementById('policiesForm');
 export const subSubcategoriesContainer = document.getElementById('subSubcategoriesContainer');
 export const adminPromoCardsManagement = document.getElementById('adminPromoCardsManagement');
 export const adminBrandsManagement = document.getElementById('adminBrandsManagement');
-
